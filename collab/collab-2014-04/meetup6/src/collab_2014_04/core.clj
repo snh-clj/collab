c69577773d79d0133386e1ba4a69a959ef395815
snh-clj collab <meetup@localhost>
2014-04-24 02:33:29 +0000

    Commiting meetup6 collaboration solution.

--- collab/collab-2014-04/meetup6/src/collab_2014_04/core.clj
+++ collab/collab-2014-04/meetup6/src/collab_2014_04/core.clj
@@ -4,7 +4,7 @@
 
 ;; Solutions by: <individual or group names here>
 
-;;; Clojure reference:
+;;i; Clojure reference:
 ;; http://ClojureDocs.org
 
 
@@ -22,57 +22,83 @@
 ;;  ^1 http://en.wikipedia.org/wiki/Roman_numerals#Subtractive_principle
 
 (defn four-clojure-92 [roman]
-  42)
+  (let [rels {\I #{\V \X}
+              \X #{\L \C}
+              \C #{\D \M}}
+        subtract? #(rels %)
+        values {\I 1
+                \V 5
+                \X 10
+                \L 50
+                \C 100
+                \D 500
+                \M 1000}]
+    (loop [total 0
+           the-last nil
+           current (first roman)
+           the-rest (rest roman)]
+      (if (nil? current)
+        total
+        (recur
+         (+ total
+            (if (subtract? the-last) (and (= the-last \I)
+                     (not (= current \I)))
 
-;; http://www.4clojure.com/problem/178
-;;
-;; Following on from Recognize Playing Cards, determine the best poker
-;; hand that can be made with five cards. The hand rankings are listed
-;; below for your convenience.
-;;
-;; * Straight flush: All cards in the same suit, and in sequence
-;; * Four of a kind: Four of the cards have the same rank
-;; * Full House: Three cards of one rank, the other two of another rank
-;; * Flush: All cards in the same suit
-;; * Straight: All cards in sequence (aces can be high or low, but not both at once)
-;; * Three of a kind: Three of the cards have the same rank
-;; * Two pair: Two pairs of cards have the same rank
-;; * Pair: Two cards have the same rank
-;; * High card: None of the above conditions are met
+              )
+            (get values current))
+         current
+         (first the-rest)
+         (rest the-rest))))))
 
-(defn four-clojure-178 [hand]
-  :fifty-two-pickup)
+  ;; http://www.4clojure.com/problem/178
+  ;;
+  ;; Following on from Recognize Playing Cards, determine the best poker
+  ;; hand that can be made with five cards. The hand rankings are listed
+  ;; below for your convenience.
+  ;;
+  ;; * Straight flush: All cards in the same suit, and in sequence
+  ;; * Four of a kind: Four of the cards have the same rank
+  ;; * Full House: Three cards of one rank, the other two of another rank
+  ;; * Flush: All cards in the same suit
+  ;; * Straight: All cards in sequence (aces can be high or low, but not both at once)
+  ;; * Three of a kind: Three of the cards have the same rank
+  ;; * Two pair: Two pairs of cards have the same rank
+  ;; * Pair: Two cards have the same rank
+  ;; * High card: None of the above conditions are met
 
-;; Magic Squares
-;;
-;; Given a three-by-three square, fill each square with a unique
-;; number 1-9 so all rows, columns and diagonals sum to 15.
-;; Each number should be used once and only once.
-;;
-;; The output of your function should return a sequence (or list or vector) of
-;; all solutions, with ;; each solution being a sequence of three sequences of
-;; three numbers, representing each row. e.g.
-;;
-;; [[[1 2 3][4 5 6][7 8 9]] [[2 3 4][5 6 7][8 9 1]] ... ]
-;; ^--- the above doesn't meet summation constraints
-;;
-;; P.S. For those already comfortable with Clojure, this problem has a
-;; nice solution using core.logic.
+  (defn four-clojure-178 [hand]
+    :fifty-two-pickup)
 
-(defn magic-squares []
-  [[[1 2 3] [4 5 6] [7 8 9]]
-   [[9 8 7] [6 5 4] [3 2 1]]
-   ,,,])
+  ;; Magic Squares
+  ;;
+  ;; Given a three-by-three square, fill each square with a unique
+  ;; number 1-9 so all rows, columns and diagonals sum to 15.
+  ;; Each number should be used once and only once.
+  ;;
+  ;; The output of your function should return a sequence (or list or vector) of
+  ;; all solutions, with ;; each solution being a sequence of three sequences of
+  ;; three numbers, representing each row. e.g.
+  ;;
+  ;; [[[1 2 3][4 5 6][7 8 9]] [[2 3 4][5 6 7][8 9 1]] ... ]
+  ;; ^--- the above doesn't meet summation constraints
+  ;;
+  ;; P.S. For those already comfortable with Clojure, this problem has a
+  ;; nice solution using core.logic.
 
-(comment
-  ;; Run this first
-  (require '[collab-2014-04.core-test :as test])
+  (defn magic-squares []
+    [[[1 2 3] [4 5 6] [7 8 9]]
+     [[9 8 7] [6 5 4] [3 2 1]]
+     ,,,])
 
-  ;; To run all tests:
-  (clojure.test/test-ns 'collab-2014-04.core-test)
+  (comment
+    ;; Run this first
+    (require '[collab-2014-04.core-test :as test])
 
-  ;; To run individual test:
-  (test/run-test four-clojure-92)
-  (test/run-test four-clojure-178)
-  (test/run-test magic-squares)
-)
+    ;; To run all tests:
+    (clojure.test/test-ns 'collab-2014-04.core-test)
+
+    ;; To run individual test:
+    (test/run-test four-clojure-92)
+    (test/run-test four-clojure-178)
+    (test/run-test magic-squares)
+    )
