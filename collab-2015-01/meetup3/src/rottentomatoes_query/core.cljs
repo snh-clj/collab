b507b79e382a50da4f643c2708edbca48222d084
snh-clj collab <meetup@localhost>
2015-01-29 03:05:21 +0000

    Committing meetup3 collaboration solutions.

--- collab-2015-01/meetup3/src/rottentomatoes_query/core.cljs
+++ collab-2015-01/meetup3/src/rottentomatoes_query/core.cljs
@@ -20,16 +20,54 @@
 
 (def api-base "http://api.rottentomatoes.com/api/public/v1.0")
 
-(def title "The Sting")
+(def title "Bacon")
 
-(defn -main [& _args]
-  (let [rq (str api-base "/movies.json" "?q=" title "&apikey=" api-key)]
+(defn title-search [query]
+  (let [rq (str api-base "/movies.json" "?q=" query "&apikey=" api-key)]
+    (println rq)
     (.get request rq
           (fn [err resp]
             (let [response-data (-> resp .-body js/JSON.parse js->clj)
                   movies (get response-data "movies")]
               (dorun (map #(println :title (get % "title") "\n\t"
+                                    :id (get % "id") "\n\t"
                                     :cast-query-url (get-in % ["links" "cast"]))
                           movies)))))))
+
+(defn make-url [movie-id verb]
+  (str api-base "/movies/" movie-id "/" verb  ".json?apikey=" api-key))
+
+(defn similar [movie-id]
+  (let [rq (make-url movie-id "similar")]
+    (.get request rq
+          (fn [err resp]
+            (let [response-data (-> resp .-body js/JSON.parse js->clj)
+                  movies (get response-data "movies")]
+              (println response-data)
+              (dorun (map #(println :title (get % "title") "\n\t"
+                                    :id (get % "id") "\n\t")
+                          movies)))))))
+
+(defn cast [movie-id]
+  (let [rq (make-url movie-id "cast")]
+    (println rq)
+    (.get request rq
+          (fn [err resp]
+            (let [response-data (-> resp .-body js/JSON.parse js->clj)
+                  cast (get response-data "cast")]
+              (println response-data)
+              (dorun (map #(println :name (get % "name") "\n\t"
+                                    :id (get % "id") "\n\t"
+                                    :characters (get % "characters"))
+                          cast)))))))
+
+(defn -main [& _args]
+  (let [verb (first _args)
+        subject (second _args)]
+    (case verb
+      "search" (title-search subject)
+      "cast" (cast subject)
+      "similar" (similar subject)
+      (println "Unkown verb"))))
 
 (set! *main-cli-fn* -main)
