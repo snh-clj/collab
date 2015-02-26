47bbabe01ad06aebc5aa597022359007b0367091
snh-clj collab <meetup@localhost>
2015-02-26 04:15:10 +0000

    Committing meetup2 collaboration solutions.

--- collab-2015-02/meetup2/dev_src/om_my/dev.clj
+++ collab-2015-02/meetup2/dev_src/om_my/dev.clj
@@ -1,6 +1,6 @@
-(ns om-my.dev)
+ns om-my.dev)
 
 (defmacro websocket-url []
   (if-let [port (Long/parseLong (System/getenv "LEIN_FIGWHEEL_PORT"))]
-    (str "ws://localhost:" port  "/figwheel-ws")
+    (str "ws://meetup.cljx.org:" port  "/figwheel-ws")
     (throw (Exception. "Missing LEIN_FIGWHEEL_PORT environment variable. Set it."))))
