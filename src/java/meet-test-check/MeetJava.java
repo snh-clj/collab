c5f43d855902dca6fb7f4bbb0a53b1092ba54f47
Tom Dolan <tdolan@sunera.com>
2015-06-18 12:13:28 +0000

    Add basic Java class to project.
    Java class will serve as the 'class under test' for
    the Java interop between:
    
     - JUnit and test.check generators
     - test.check testing Java methods
    
    --Tom

---
+++ src/java/meet-test-check/MeetJava.java
@@ -0,0 +1,17 @@
+package meet_test_check;
+
+/**
+  JavaMeet is a basic Java class whose methods will be tested by
+  both JUnit and Clojure's test.check.
+
+ **/
+
+public class MeetJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("If I could by the world a Coke, ...");
+
+    }
+
+}
