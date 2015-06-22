5973a72e904e12d4a8a5aefd41a47ceece6dba87
Tom Dolan <tdolan@sunera.com>
2015-06-22 23:38:27 +0000

    Add main  and use int[] instead of ArrayList.
    Adding a simple implementation of main and altered
    the xformIntArray method to work on primitive int arrays
    vs. utilizing Java's ArrayList object.
    
    I felt using the int[] was closer to what a Clojure impl
    would look like and wanted to employ the most basic code
    I could.
    
    --Tom

--- src/java/meet-test-check/MeetJava.java
+++ src/java/meet-test-check/MeetJava.java
@@ -1,24 +1,32 @@
 package meet_test_check;
 
+import java.util.Arrays;
 /**
   JavaMeet is a basic Java class whose methods will be tested by
   both JUnit and Clojure's test.check.
 
- **/
+ */
 
 public class MeetJava {
 
     public static void main(String[] args) {
 
-        System.out.println("If I could by the world a Coke, ...");
+        int[] incoming = { 399328, 8379938, 588377027 };
+        MeetJava mj = new MeetJava();
+        int[] result = mj.xformIntArray(incoming);
 
+        System.out.println("Original array contents: " + Arrays.toString(incoming));
+        System.out.println("Transformed array contents: " + Arrays.toString(result));
     }
 
-    public ArrayList<Integer> xformIntArray (ArrayList<Integer> incoming) {
-        ArrayList<Integer> outgoing = new ArrayList<Integer>();
+    public int[] xformIntArray (int[] incoming) {
 
-        for (Integer in : incoming) {
-            outgoing.add(in * (in % 11));
+        int[] outgoing = new int[incoming.length];
+
+        int i = 0;
+        while (i < incoming.length) {
+            outgoing[i] = incoming[i] * (incoming[i] % 11);
+            i++;
         }
 
         return outgoing;
