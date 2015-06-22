717aded2314cce0f1a4ee8785dd6fd70aa7d1546
Tom Dolan <tdolan@sunera.com>
2015-06-22 23:43:47 +0000

    Add JUnit test for MeetJava.
    Committing base JUnit test.
    
    Subsequent commits will add the parameterization
    of this test and flush out the test itself.
    
    --Tom

---
+++ test/java/meet_test_check/MeetJavaTest.java
@@ -0,0 +1,19 @@
+package meet_test_check;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MeetJavaTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    public void testXformIntArray() {
+        fail("Not yet implemented.");
+    }
+
+}
