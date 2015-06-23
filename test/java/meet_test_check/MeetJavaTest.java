6cf2396fc0b335e9d1040964295ea20aebf20b14
Tom Dolan <tdolan@sunera.com>
2015-06-23 10:37:33 +0000

    Add test for the MeetJava.xformIntArray method.
    Initial test simply checks that the size of the 'input' array
    is the same as that of the 'xformed' array.
    
    To run the test at command line, execute the run-junit-tests.sh
    script.
    
    --Tom

--- test/java/meet_test_check/MeetJavaTest.java
+++ test/java/meet_test_check/MeetJavaTest.java
@@ -1,19 +1,37 @@
 package meet_test_check;
 
+import java.lang.Math;
+
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import meet_test_check.MeetJava;
+
 public class MeetJavaTest {
 
+    private static int[] testArray;
+    
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        testArray = initializeArray();
     }
 
     @Test
     public void testXformIntArray() {
-        fail("Not yet implemented.");
+        int[] xformedArray = MeetJava.xformIntArray(testArray);
+        assertTrue(xformedArray.length == testArray.length);
     }
 
+    private static int[] initializeArray() {
+        int length = 100;
+        int upperLimit = 5332232;
+        int[] testData = new int[length];
+        for (int i = 0; i < length; i++) {
+            //create an integer between 0 and value of upperLimit (inclusive)
+            testData[i] = (int) Math.random() * (upperLimit +1);
+        }
+        return testData;
+    }
 }
