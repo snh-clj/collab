d00846e02c236a0368766f731c3eb063d1e0dbe0
Tom Dolan <tdolan@sunera.com>
2015-06-23 12:26:59 +0000

    Add test.check test for the MeetJava Java class.
    Committing initial test for MeetJava.
    The test checks that all elements are positive. This test
    is to identify when/why positive int values result in negative
    value when run through xformIntArray method.
    
    TODO: Need to update generator so that only positive integers
    are generated.
    
    --Tom

---
+++ test/meet_test_check/meet_java_test.clj
@@ -0,0 +1,17 @@
+(ns meet-test-check.meet-java-test
+  (:require [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]])
+  (:import (meet_test_check MeetJava)))
+
+
+(defspec all-ints-positive
+  10
+  (prop/for-all [v (gen/vector gen/int)]
+                (let [xformed-array (MeetJava/xformIntArray (int-array v))]
+                  (every? pos? xformed-array))))
+#_(defspec all-ints-positive
+  100
+  (prop/for-all [v (gen/vector gen/int)]
+     (every? pos? v)))
