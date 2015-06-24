2cc7ce20d0a2b84ab34d663aecdcbb9fc8f8b928
Tom Dolan <tdolan@sunera.com>
2015-06-24 12:25:20 +0000

    Add 'allPositive' test to JUnit test class.
    --Tom

--- test/java/meet_test_check/MeetJavaTest.java
+++ test/java/meet_test_check/MeetJavaTest.java
@@ -1,6 +1,7 @@
 package meet_test_check;
 
 import java.lang.Math;
+import java.lang.Integer;
 
 import static org.junit.Assert.*;
 
@@ -24,9 +25,22 @@ public class MeetJavaTest {
         assertTrue(xformedArray.length == testArray.length);
     }
 
+    @Test
+    public void allItemsPositive() {
+        boolean allItemsPositive = true;
+        int[] xformedArray = MeetJava.xformIntArray(testArray);
+        for (int i = 0; i < xformedArray.length; i++) {
+            if (xformedArray[i] < 0) {
+                allItemsPositive = false;
+            }
+        }
+        assertTrue(allItemsPositive);
+    }
+
+
     private static int[] initializeArray() {
         int length = 100;
-        int upperLimit = 5332232;
+        int upperLimit = Integer.MAX_VALUE;
         int[] testData = new int[length];
         for (int i = 0; i < length; i++) {
             //create an integer between 0 and value of upperLimit (inclusive)
