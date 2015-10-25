53118244aca46604ae839a86dc2944272f87590a
Aaron Brooks <aaron@brooks1.net>
2015-10-24 21:46:26 -0400

    collab-2015-10: Redo tests to be data driven.
    This allows us to meta program a bit and generate both the tests and
    have a function which runs through a repl friendly 'run-tests
    function derived from the same set of data.

--- collab-2015-10/meetup0/test/collab_2015_10/core_test.clj
+++ collab-2015-10/meetup0/test/collab_2015_10/core_test.clj
@@ -1,89 +1,46 @@
 (ns collab-2015-10.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest testing is]]
             [collab-2015-10.core :refer [my-eval]]))
 
-(deftest numbers-test
-  (testing "Numbers eval to themselves"
-    (is (= 1
-           (my-eval {} 1)))))
+(def test-cases
+  '[["Numbers eval to themselves" (my-eval {} 1) 1]
+    ["Strings eval to themselves" (my-eval {} "hello") "hello"]
+    ["Symbols eval to their value in the environment" (my-eval {'x 10} 'x) 10]
+    ["Symbols eval to their value in the environment" (my-eval {'x "yo!"} 'x) "yo!"]
+    ["Functions evaluate to themselves" (my-eval {} +) +]
+    ["Functions evaluate to themselves" (my-eval {} *) *]
+    ["If statements with nil test eval else" (my-eval {} '(if nil 0 1)) 1]
+    ["If statements with non-nil test eval then" (my-eval {'test 1, 'x 0, 'y 1} '(if test x y)) 0]
+    ["Empty do statement evals to nil" (my-eval {} '(do)) nil]
+    ["One-expression do evals to expression" (my-eval {} '(do 1)) 1]
+    ["Multi-expression do evals to last expression" (my-eval {} '(do 1 2 3 4)) 4]
+    ["Let statements add to environment" (my-eval {} '(let [x 1] x)) 1]
+    ["Function application" (my-eval {'+ +} '(+ 1 1)) 2]
+    ["Function application" (my-eval {'/ /} '(/ 20 4)) 5]
+    ["Ensure we're calling my-eval recursively" (my-eval {'+ +} '(do (+ 1 2))) 3]
+    ["Ensure we're calling my-eval recursively" (my-eval {'+ +} '(+ (+ 1 10) 2)) 13]
+    ["Do expressions evaluate all forms" (my-eval {'deref deref, 'reset! reset!, 'a (atom nil), '+ +} '(do (reset! a 10) (+ (deref a) 75))) 85]])
 
-(deftest strings-test
-  (testing "Strings eval to themselves"
-    (is (= "hello"
-           (my-eval {} "hello")))))
+(doseq [[s f r n] (map conj test-cases (range))]
+    (eval
+     `(deftest ~(symbol (str "test-" (inc n)))
+       (testing ~s
+         (eval (is (= ~r ~f)))))))
 
-(deftest symbols-test-1
-  (testing "Symbols eval to their value in the environment"
-    (is (= 10
-           (my-eval {'x 10} 'x)))))
-
-(deftest symbols-test-2
-  (testing "Symbols eval to their value in the environment"
-    (is (= "yo!"
-           (my-eval {'x "yo!"} 'x)))))
-
-(deftest function-test-1
-  (testing "Functions evaluate to themselves"
-    (is (= +
-           (my-eval {} +)))))
-
-(deftest function-test-2
-  (testing "Functions evaluate to themselves"
-    (is (= *
-           (my-eval {} *)))))
-
-(deftest if-nil-test
-  (testing "If statements with nil test eval else"
-    (is (= 1
-           (my-eval {} '(if nil 0 1))))))
-
-(deftest if-non-nil-test
-  (testing "If statements with non-nil test eval then"
-    (is (= 0
-           (my-eval {'test 1 'x 0 'y 1} '(if test x y))))))
-
-(deftest empty-do-test
-  (testing "Empty do statement evals to nil"
-    (is (= nil
-           (my-eval {} '(do))))))
-
-(deftest one-do-test
-  (testing "One-expression do evals to expression"
-    (is (= 1
-           (my-eval {} '(do 1))))))
-
-(deftest last-do-test
-  (testing "Multi-expression do evals to last expression"
-    (is (= 4
-           (my-eval {} '(do 1 2 3 4))))))
-
-(deftest let-test
-  (testing "Let statements add to environment"
-    (is (= 1
-           (my-eval {} '(let [x 1] x))))))
-
-(deftest function-application-test-1
-  (testing "Function application"
-    (is (= 2
-           (my-eval {'+ +} '(+ 1 1))))))
-
-(deftest function-application-test-2
-  (testing "Function application"
-    (is (= 5
-           (my-eval {'/ /} '(/ 20 4))))))
-
-(deftest recursive-test-1
-  (testing "Ensure we're calling my-eval recursively"
-    (is (= 3
-           (my-eval {'+ +} '(do (+ 1 2)))))))
-
-(deftest recursive-test-2
-  (testing "Ensure we're calling my-eval recursively"
-    (is (= 13
-           (my-eval {'+ +} '(+ (+ 1 10) 2))))))
-
-(deftest do-all-test
-  (testing "Do expressions evaluate all forms"
-    (is (= 85
-           (my-eval {'deref deref 'reset! reset! 'a (atom nil) '+ +}
-           '(do (reset! a 10) (+ (deref a) 75)))))))
+(defn run-tests []
+  (loop [tests (map conj test-cases (range))]
+    (if tests
+      (let [[test & next-tests] tests
+            [s f r n] test
+            result (eval `~f)]
+        (print  "Test" (inc n) "-" (str s ": "))
+        (if (= (eval r) result)
+          (do
+            (println "PASSED")
+            (recur next-tests))
+          (do
+            (println "FAILED")
+            (println "  We ran:" f)
+            (println "    expecting:" r)
+            (println "    but got:" result))))
+      (println "All tests passed: YAY!"))))
