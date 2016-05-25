6e3bd9bd7ce7cab8b539cd6e29523b2be59df4ad
Matt Oquist <moquist@majen.net>
2016-05-25 04:31:50 +0000

    Redact collab-2016-05 solutions.

--- collab-2016-05/meetup0/src/ecapseman/core.clj
+++ collab-2016-05/meetup0/src/ecapseman/core.clj
@@ -10,7 +10,7 @@
 
   Restriction: range"
   [x y]
-  (take (- y x) (iterate inc x)))
+  ::no-implementation)
 
 (defn four-clojure-29
   "https://www.4clojure.com/problem/29 -- Get the Caps
@@ -20,7 +20,7 @@
 
   Write a function which takes a string and returns a new string containing only the capital letters."
   [s]
-  (apply str (re-seq #"[A-Z]" s)))
+  ::no-implementation)
 
 (defn four-clojure-77
   "https://www.4clojure.com/problem/77 -- Anagram Finder
@@ -35,20 +35,7 @@
   words. Words without any anagrams should not be included in the
   result."
   [words]
-  ;; Wow... I don't remember how long ago I submitted this to
-  ;; 4clojure, but I think it has not been golfed. :-)
-  (set
-   (map (fn find-1 [[k v]] (set v))
-        (filter (fn find-2 [[k v]] (< 1 (count v)))
-                ((fn find-3 [words coll]
-                   (if (empty? words)
-                     coll
-                     (let [word (first words)
-                           words (rest words)]
-                       (if (coll (set word))
-                         (recur words (merge-with concat {(set word) [word]} coll))
-                         (recur words (assoc coll (set word) [word]))))))
-                 words {})))))
+  ::no-implementation)
 
 (defn project-euler-4-prep
   "Write a predicate function that takes a number and returns a truthy
@@ -65,11 +52,7 @@
   => false
   "
   [n]
-  (loop [n (str n)]
-    (if (empty? n)
-      true
-      (when (= (first n) (last n))
-        (recur (butlast (drop 1 n)))))))
+  ::no-implementation)
 
 (defn project-euler-4
   "https://projecteuler.net/problem=4
@@ -83,9 +66,7 @@
   Write a function that calculates the largest palindrome made from
   the product of two 3-digit numbers."
   []
-  (reduce max 0 (filter project-euler-4-prep (for [x (range 100 1000)
-                                                   y (range 100 1000)]
-                                               (* x y)))))
+  ::no-implementation)
 
 (defn four-clojure-84
   "https://www.4clojure.com/problem/84 -- Transitive Closure
@@ -98,71 +79,4 @@
   relation ( http://en.wikipedia.org/wiki/Binary_relation ). The
   relation will be represented as a set of 2 item vectors."
   [s]
-  (let [m (into {} s)]
-     (reduce (fn [r [k v]]
-               (let [r (conj r [k v])]
-                 (if (m v)
-                   (recur r [k (m v)])
-                   r)))
-             #{}
-             m)))
-
-(comment
-  (require 'ecapseman.core :reload-all) (in-ns 'ecapseman.core) (use 'clojure.repl)
-  )
-
-
-(defn euler-4-answers [x y]
-  (let [palindrome? project-euler-4-prep]
-    (condp = [x y]
-      ;; 9009
-      [2 2]
-      (reduce max 0 (filter palindrome? (for [x (range 10 100)
-                                              y (range 10 100)]
-                                          (* x y))))
-      ;; 828828 .5s
-      [3 2]
-      (reduce max 0 (filter palindrome? (for [x (range 10 100)
-                                              y (range 10 100)
-                                              z (range 10 100)]
-                                          (* x y z))))
-
-      ;; 67144176 52s
-      [4 2]
-      (reduce max 0 (filter palindrome? (for [x (range 10 100)
-                                              y (range 10 100)
-                                              z (range 10 100)
-                                              a (range 10 100)
-                                              ]
-                                          (* x y z a))))
-
-      ;; 906609
-      [2 3]
-      (reduce max 0 (filter palindrome? (for [x (range 100 1000)
-                                              y (range 100 1000)]
-                                          (* x y))))
-
-      ;; 99000099 60s
-      [2 4]
-      (reduce max 0 (filter palindrome? (for [x (range 1000 10000)
-                                              y (range 1000 10000)]
-                                          (* x y))))
-
-      [3 3]
-      (reduce max 0 (filter palindrome? (for [x (range 100 1000)
-                                              y (range 100 1000)
-                                              z (range 100 1000)
-                                              ]
-                                          (* x y z))))
-
-      [4 3]
-      (reduce max 0 (filter palindrome? (for [x (range 100 1000)
-                                              y (range 100 1000)
-                                              z (range 100 1000)
-                                              a (range 100 1000)
-                                              ]
-                                          (* x y z a))))
-
-
-      )))
-
+  ::no-implementation)
