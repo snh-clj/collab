bfd2b82ba9f9ea34f92025d133a1f98c00988b35
snh-clj collab <meetup@localhost>
2017-06-29 02:37:02 +0000

    Committing meetup3 collab-2017-06/ solutions.

--- collab-2017-06/meetup3/project.clj
+++ collab-2017-06/meetup3/project.clj
@@ -4,8 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [http-kit "2.2.0"]
+                 [hiccup "1.0.5"]
                  [com.tbaldridge/odin "0.2.0"]
                  [com.rpl/specter "1.0.2"]
                  [enlive "1.1.6"]
+                 [org.clojure/data.xml "0.0.8"]
                  [seesaw "1.4.5"]
                  [datascript "0.16.1"]])
--- collab-2017-06/meetup3/src/collab_2017_06/core.clj
+++ collab-2017-06/meetup3/src/collab_2017_06/core.clj
@@ -1,4 +1,50 @@
-(ns collab-2017-06.core)
+(ns collab-2017-06.core
+  (:require [org.httpkit.client :as http]
+            [hiccup.core :as hic]
+            [clojure.data.xml :as xml]
+            [net.cgrand.enlive-html :as html]
+            [net.cgrand.tagsoup :as tagsoup]
+            [clojure.java.io :as io]
+            ))
+
+(defn string->stream
+  ([s] (string->stream s "UTF-8"))
+  ([s encoding]
+     (-> s
+         (.getBytes encoding)
+         (java.io.ByteArrayInputStream.))))
+
+(def ^:dynamic *base-url* "http://www.online-literature.com/")
+
+(defn fetch-url [url]
+    (html/html-resource (java.net.URL. url)))
+
+(defn string->nodes [s]
+  (-> s
+      string->stream
+      (html/get-resource tagsoup/parser)))
+
+(defn get-lit []
+  (let [top-site (:body @(http/get "http://www.online-literature.com/"))
+        ;; site-stream (string->stream top-site)
+        ;; nodes (html/get-resource site-stream tagsoup/parser)
+        nodes (string->nodes top-site)
+        authors (html/select nodes [:table.table-striped :tr :td :a])
+        author->url (map #(let [{:keys [title href]} (:attrs %)] [title href]) authors)
+        author-page-futures (map (fn [[author url]] [author (http/get url)])
+                                 author->url)
+        author-pages (map (fn [[author http-get]]
+                            [author (-> http-get deref :body string->nodes)])
+                          author-page-futures)
+        ]
+    author-pages
+    ))
+
+
+(comment
+  (clojure.pprint/pprint (->> mef first :content (map :content) (map #(-> % first :attrs :href)))))
+
+
 
 (defn foo
   "I don't do a whole lot."
