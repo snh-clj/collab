0cc365282eba5c5a1b7dfdc933dae363d77159a6
snh-clj collab <meetup@localhost>
2017-06-29 02:36:59 +0000

    Committing meetup2 collab-2017-06/ solutions.

--- collab-2017-06/meetup2/src/collab_2017_06/core.clj
+++ collab-2017-06/meetup2/src/collab_2017_06/core.clj
@@ -1,6 +1,32 @@
-(ns collab-2017-06.core)
+(ns collab-2017-06.core
+  (:require [clojure.zip :as z]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def nds "Our fancy, nested data structure"
+  {:a [1 2 3 #{:a :b :c '(x1 y1 z1)}]
+   :b [4 5 6 #{:d :e :f '(x2 y2 z2)}]})
+
+
+(def znds "Our zipper around our fancy, nested data structure"
+  (z/zipper coll? ;; branch predicate
+            seq   ;; get children function
+            (fn make-node [existing-node children]
+              (prn :chilren children)
+              (if (map-entry? existing-node)
+                (vec children)
+                (into (empty existing-node)
+                      children)))
+            nds))
+
+
+(def znode "A node deep down in our zippered, fancy, nested data structure"
+  (-> znds
+    z/down
+    z/right
+    z/down
+    z/right
+    z/down
+    z/right
+    z/right
+    z/right
+    z/down))
