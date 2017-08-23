0b14a16cc507139262a91a8e3511a910f97e9dd5
Matt Oquist <moquist@majen.net>
2017-08-23 18:04:09 -0400

    Add meetup 2017-08 collab.

---
+++ collab-2017-08/meetup5/README.md
@@ -0,0 +1,7 @@
+# four-clojure-four-fun
+
+Suggested problems:
+
+1. Easy: http://www.4clojure.com/problem/32
+1. Medium: http://www.4clojure.com/problem/44
+1. Hard: http://www.4clojure.com/problem/94
---
+++ collab-2017-08/meetup8/test/four_clojure_four_fun/core_test.clj
@@ -0,0 +1,7 @@
+(ns four-clojure-four-fun.core-test
+  (:require [clojure.test :refer :all]
+            [four-clojure-four-fun.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
