dc3592fe8a3666d4c3c8164e90cb2e37a0812dbe
snh-clj collab <meetup@localhost>
2017-08-24 03:02:47 +0000

    Committing meetup1 collabl-2017-08/ solutions

--- collab-2017-08/meetup1/src/four_clojure_four_fun/core.clj
+++ collab-2017-08/meetup1/src/four_clojure_four_fun/core.clj
@@ -1,6 +1,25 @@
 (ns four-clojure-four-fun.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn problem-32 [x]
+  (mapcat vector x x))
+
+(defn problem-44-1 [n coll]
+  (cond
+   (pos? n)
+   (recur (dec n) (concat (drop 1 coll) (take 1 coll)))
+
+   (neg? n)
+   (recur (inc n) (concat [(last coll)] (butlast coll)))
+
+   (zero? n)
+   coll
+
+   :anything
+   (throw (ex-info "Holy cow, we got a thing that's not positive, negative, or zero!" {}))))
+
+(defn problem-44 [n coll]
+  (let [c (count coll)]
+    (take c
+          (drop (mod n c)
+                (cycle coll))))
+  )
--- collab-2017-08/meetup1/test/four_clojure_four_fun/core_test.clj
+++ collab-2017-08/meetup1/test/four_clojure_four_fun/core_test.clj
@@ -1,7 +1,18 @@
 (ns four-clojure-four-fun.core-test
-  (:require [clojure.test :refer :all]
-            [four-clojure-four-fun.core :refer :all]))
+ (:require [clojure.test :refer :all]
+           [four-clojure-four-fun.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest problem-32-test
+  (let [__ core/problem-32]
+    (is (= (__ [1 2 3]) '(1 1 2 2 3 3)))
+    (is (= (__ [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
+    (is (= (__ [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+    (is (= (__ [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))))
+
+(deftest problem-44-test
+  (let [__ core/problem-44]
+    (is (= (__ 2 [1 2 3 4 5]) '(3 4 5 1 2)))
+    (is (= (__ -2 [1 2 3 4 5]) '(4 5 1 2 3)))
+    (is (= (__ 6 [1 2 3 4 5]) '(2 3 4 5 1)))
+    (is (= (__ 1 '(:a :b :c)) '(:b :c :a)))
+    (is (= (__ -4 '(:a :b :c)) '(:c :a :b)))))
