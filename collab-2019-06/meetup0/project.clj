9d2cebfe84095a7fd2eebb5d68e7363ee1a785d9
Aaron Brooks <aaron@brooks1.net>
2019-06-24 13:55:16 -0400

    [collab-2019-06] Add initial bermuda project and base board/tile data.

---
+++ collab-2019-06/meetup0/project.clj
@@ -0,0 +1,8 @@
+(defproject bermuda "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main bermuda.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [zprint "0.4.16"]])
