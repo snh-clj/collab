6eaa174374092b1d8b6e2e9a1c853599d03f7901
Tom Dolan <tgdolan@gmail.com>
2019-06-26 21:26:35 -0400

    Start at bermuda triangle problem.

--- collab-2019-06/meetup1/src/bermuda/core.clj
+++ collab-2019-06/meetup1/src/bermuda/core.clj
@@ -8,6 +8,11 @@
    [:GN :WT :GN :GN]
    [:YL :WT :RD :WT]])
 
+(def corners
+ [[:GN :BK :GN :WT]
+  [:GN :GN :YL :WT]
+  [:RD :WT :BL :RD]])
+
 (def tiles
   [[:RD :GN :YL]
    [:BK :BK :GN]
@@ -25,6 +30,40 @@
    [:BK :BL :YL]
    [:WT :BL :BL]
    [:BK :RD :GN]])
+
+(defn cull-tile
+  [tile tiles]
+  (let [count (get tiles tile)]
+    (if (> count 1)
+      (assoc tiles tile (dec count))
+      (dissoc tiles tile))))
+(defn tile-has-correct-dots?
+  [x y]
+  true)
+
+(defn pick-tile
+  [tiles match-vector]
+  (for [[tile count] tiles
+       orientation [0 1 2]
+    :when (tile-has-correct-dots? tile orientation)
+    :when (> count 0)]
+    [tile orientation (cull-tile tile tiles)]
+  ))
+
+(defn find-match
+  [a b c d e f]
+  "fooo")
+
+(defn find-corner
+  [edge tiles-rest]
+  (for [[tile-0 orient-0 tiles-rest-0] (pick-tile (subvec edge 1 3) tiles-rest)
+        [tile-1 orient-1 tiles-rest-1] (pick-tile (subvec edge 0 1) tiles-rest-0)
+        [tile-2 orient-2 tiles-rest-2] (pick-tile (subvec edge 3 4) tiles-rest-1)
+        :let [t3-match (find-match orient-1 orient-2 orient-3 tile-1 tile-2 tile-3)]
+        [tile-3 orient-3 tiles-rest-3] (pick-tile t3-match tiles-rest)]
+
+        [tiles-rest-3 [[tile-0 orient-0] [tile-1 orient-1] [tile-2 orient-2]] [tile-3 orient-3]]))
+
 
 (defn symmetry [tls]
   (map (fn [[a b c]] #{[a b c] [b c a] [c a b]}) tls))
